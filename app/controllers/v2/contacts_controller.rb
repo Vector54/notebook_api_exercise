43c6f8626d3f392b54890048445060f88bf41262
Victor de Oliveira <oliveira.victor.51200@gmail.com>
2022-10-25 11:26:50 -0300

    feat: Makes a service for generating the pagination links for the header
    Unfortunately, api-paginator isnt compatible with json-api, so i had to make my own service.

--- app/controllers/v2/contacts_controller.rb
+++ app/controllers/v2/contacts_controller.rb
@@ -4,7 +4,9 @@ module V2
 
     # GET /contacts
     def index
-      @contacts = Contact.all.page(params[:page]).per(10)
+      @contacts = Contact.all.page(params[:page]).per(15)
+
+      response.set_header('Links', HeaderPaginator.call(@contacts, v2_contacts_url))
 
       render jsonapi: @contacts
     end
